8a54303e3cfd5706a6a02310d15442338dab3a7c
Houtvie <houtviehub@gmail.com>
2025-08-29 11:24:04 +0200

    New products

--- data/products.js
+++ data/products.js
@@ -10,14 +10,51 @@ export default function products(){
       image: "SewingMachine.jpeg",
     },
     {
-      name: "Air Conditional",
+      name: "Air Conditioner",
       unitPrice: 79900,
       quantityInStock: 18,
       reorderLevel: 9,
       category: "appliances",
       description: "description",
       image: "AC.jpeg",
+    },
+     {
+      name: "Television",
+      unitPrice: 69400,
+      quantityInStock: 2,
+      reorderLevel: 9,
+      category: "appliances",
+      description: "description",
+      image: "TV.jpeg",
+    },
+     {
+      name: "Blender",
+      unitPrice: 29500,
+      quantityInStock: 27,
+      reorderLevel: 9,
+      category: "appliances",
+      description: "description",
+      image: "Blender.jpeg",
+    },
+     {
+      name: "Ceiling Fan",
+      unitPrice: 30000,
+      quantityInStock: 10,
+      reorderLevel: 9,
+      category: "appliances",
+      description: "description",
+      image: "Fan.jpeg",
+    },
+     {
+      name: "Fridge",
+      unitPrice: 87500,
+      quantityInStock: 7,
+      reorderLevel: 9,
+      category: "appliances",
+      description: "description",
+      image: "Fridge.jpeg",
     }
+
   ]
 
   return products;
